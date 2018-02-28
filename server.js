8fe5a2c2190fe522cff6c0ef132c4f757fd2d8bb
Mario <stepenik@gmail.com>
2018-02-28 23:15:07 +0100

    Add project page

--- server.js
+++ server.js
@@ -47,6 +47,11 @@ app.get('/about', (req, res) =>{
   });
 });
 
+app.get('/projects', (req, res) =>{
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  })
+});
 
 app.get('/bad', (req, res) =>{
   res.send({
